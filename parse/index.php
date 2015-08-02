a764f79f7532934a2a9fc5b0960cbedcb72f1a63
Wico Wen <is@wico.cc>
2015-08-02 16:08:55 +0800

    add object and query

--- parse/index.php
+++ parse/index.php
@@ -3,7 +3,7 @@
     include_once "lib/php/vendor/autoload.php";
 
     use Parse\ParseObject;
-    #use Parse\ParseQuery;
+    use Parse\ParseQuery;
     #use Parse\ParseACL;
     #use Parse\ParsePush;
     #use Parse\ParseUser;
@@ -16,8 +16,30 @@
 
     ParseClient::initialize($PARSE_APP_ID, $PARSE_REST_KEY, $PARSE_MASTER_KEY);
 
-    $testObject = ParseObject::create("TestObject");
-    $testObject->set("foo", "bar");
-    $testObject->save();
+    /*
+    $object = ParseObject::create("TestObject");
+    $objectId = $object->getObjectId();
+    $php = $object->get("elephant");
 
+    // Set values:
+    $object->set("elephant", "php");
+    $object->set("today", new DateTime());
+    $object->setArray("mylist", [1, 2, 3]);
+    $object->setAssociativeArray(
+        "languageTypes", array("php" => "awesome", "ruby" => "wtf")
+    );
+    // Save:
+    $object->save();
+    */
 
+$query = new ParseQuery("TestObject");
+
+// Get a specific object:
+$object = $query->get("zD67yQOxAi");
+
+$query->limit(10); // default 100, max 1000
+
+// Just the first result:
+$first = $query->first();
+
+var_dump($first);
