cf3e3c9162ce3aea3e9b2190046d173a73e5b5a2
Wico Wen <is@wico.cc>
2018-03-20 18:38:22 +0800

    update statsroyale crawler can run in python 3.6

--- python/crawler/web-statsroyale-tournaments.py
+++ python/crawler/web-statsroyale-tournaments.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*
+# -*- coding: utf-8 -
 
-import urllib2
-import HTMLParser
+###
+# run command
+# $ watch ./web-statsroyale-tournaments.py
+#
+# that can get now free to join tournaments
+###
+
+import urllib.request
 import lxml.html
 from lxml.cssselect import CSSSelector
 from bs4 import BeautifulSoup
@@ -12,17 +18,16 @@ request_headers = {
     "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.119 Safari/537.36"
 }
 
-request = urllib2.Request(url, headers=request_headers)
-contents = urllib2.urlopen(request).read()
+request = urllib.request.Request(url, headers=request_headers)
+contents = urllib.request.urlopen(request).read()
 
 root = lxml.html.fromstring(contents)
-sel = CSSSelector('div .challenges__rowContainer')
+sel = CSSSelector("div .challenges__rowContainer")
 results = sel(root)
 
-htmlParser = HTMLParser.HTMLParser()
 for result in results:
-    trimContent = lxml.html.tostring(result).strip(' \t\n\r')
-    soup=BeautifulSoup(trimContent, 'lxml')
+    trimContent = lxml.html.tostring(result)
+    soup=BeautifulSoup(trimContent, "lxml")
     text = soup.get_text(" | ", strip=True)
     infos = text.split(" | ")
     infos.pop(1)
@@ -32,6 +37,6 @@ for result in results:
 
     players = infos[1]
     if players != "50/50":
-        print ' | '.join(infos).encode('utf-8').strip()
-
+        info = " | "
+        print(info.join(infos))
 
